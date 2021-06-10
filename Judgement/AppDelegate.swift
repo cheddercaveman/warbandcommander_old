72792c6f575b4b79f7afff37499a74f0875a46e3
Chad Kaufman <ckaug@arity.com>
2021-06-09 21:55:51 -0500

    Added and Removed a lot of stuff. Should be the final build of this app pre-v2 formal release

--- Judgement/AppDelegate.swift
+++ Judgement/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
 
         UIApplication.shared.isIdleTimerDisabled = true
         self.window?.tintColor = UIColor(red:0.43, green:0.63, blue:0.76, alpha:1.0)
